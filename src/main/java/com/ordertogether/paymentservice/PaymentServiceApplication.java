dfc2d5c6673a3f7726c098e37dba934fd0afdbdd
나제법 <skwpqjq@gmail.com>
2025-12-18 01:21:27 +0900

    feat: BaseTimeEntity 추가

--- src/main/java/com/ordertogether/paymentservice/PaymentServiceApplication.java
+++ src/main/java/com/ordertogether/paymentservice/PaymentServiceApplication.java
@@ -2,7 +2,9 @@ package com.ordertogether.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class PaymentServiceApplication {
 
