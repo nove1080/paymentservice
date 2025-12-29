50b99624ecfba5e6d2e24d82fe5c9f328b5d90ff
나제법 <skwpqjq@gmail.com>
2025-12-30 00:12:28 +0900

    feat: 결제 이벤트와 결제 상품 연관관계 설정

--- src/main/java/com/ordertogether/paymentservice/payment/domain/PaymentOrder.java
+++ src/main/java/com/ordertogether/paymentservice/payment/domain/PaymentOrder.java
@@ -7,17 +7,19 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import java.math.BigDecimal;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import lombok.experimental.SuperBuilder;
 
 @ToString(callSuper = true)
 @Getter
-@SuperBuilder
+@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Entity
@@ -28,8 +30,9 @@ public class PaymentOrder extends BaseTimeEntity {
     @Column(name = "payment_order_id", updatable = false)
     private Long id;
 
-    @Column(nullable = false)
-    private Long paymentEventId;
+    @ManyToOne
+    @JoinColumn(name = "payment_event_id")
+    private PaymentEvent paymentEvent;
 
     @Column(nullable = false)
     private Long sellerId;
@@ -44,4 +47,7 @@ public class PaymentOrder extends BaseTimeEntity {
     @Enumerated(EnumType.STRING)
     private PaymentStatus paymentStatus;
 
+    public void assignPaymentEvent(PaymentEvent paymentEvent) {
+        this.paymentEvent = paymentEvent;
+    }
 }
