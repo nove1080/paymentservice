329f2f563ed3705881cc542e23fdc1b99ea6e343
나제법 <skwpqjq@gmail.com>
2026-01-08 23:59:52 +0900

    feat: PaymentStatus 결제 상태 전이 규칙 추가
    - 상태 전이 검증 로직 구현

--- src/main/java/com/ordertogether/paymentservice/payment/domain/PaymentStatus.java
+++ src/main/java/com/ordertogether/paymentservice/payment/domain/PaymentStatus.java
@@ -1,10 +1,12 @@
 package com.ordertogether.paymentservice.payment.domain;
 
+import java.util.Collections;
+import java.util.List;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
-@ToString
+@ToString(exclude = "nextStatuses")
 @Getter
 @RequiredArgsConstructor
 public enum PaymentStatus {
@@ -15,8 +17,23 @@ public enum PaymentStatus {
     UNKNOWN("알 수 없음");
 
     private final String description;
+    private List<PaymentStatus> nextStatuses;
+
+    static {
+        NOT_STARTED.nextStatuses = List.of(EXECUTING, FAIL, UNKNOWN);
+        EXECUTING.nextStatuses = List.of(SUCCESS, FAIL, UNKNOWN);
+        SUCCESS.nextStatuses = Collections.emptyList();
+        FAIL.nextStatuses = Collections.emptyList();
+        UNKNOWN.nextStatuses = List.of(EXECUTING);
+    }
 
     public boolean isNotStarted() {
         return this == NOT_STARTED;
+    }
+
+    public void validateTransitionTo(PaymentStatus nextStatus) {
+        if (!nextStatuses.contains(nextStatus)) {
+            throw new IllegalStateException("결제 상태는 %s 에서 %s(으)로 변경이 불가능합니다.".formatted(this.name(), nextStatus.name()));
+        }
     }
 }
---
+++ src/test/java/com/ordertogether/paymentservice/payment/domain/PaymentStatusTest.java
@@ -0,0 +1,54 @@
+package com.ordertogether.paymentservice.payment.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Named;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+@DisplayName("결제 상태 테스트")
+class PaymentStatusTest {
+
+    @Nested
+    @DisplayName("상태 전이 시")
+    class whenTransit {
+        @DisplayName("잘못된 상태 전이면 예외를 던진다")
+        @MethodSource("invalidTransitions")
+        @ParameterizedTest(name = "{0} -> {1}")
+        void thenThrowException(PaymentStatus current, PaymentStatus next) {
+            // when & then
+            assertThatThrownBy(() -> current.validateTransitionTo(next))
+                .isInstanceOf(IllegalStateException.class);
+        }
+
+        static Stream<Arguments> invalidTransitions() {
+            return Stream.of(
+                namedArguments(PaymentStatus.NOT_STARTED, PaymentStatus.SUCCESS),
+                namedArguments(PaymentStatus.EXECUTING, PaymentStatus.NOT_STARTED),
+                namedArguments(PaymentStatus.SUCCESS, PaymentStatus.NOT_STARTED),
+                namedArguments(PaymentStatus.SUCCESS, PaymentStatus.EXECUTING),
+                namedArguments(PaymentStatus.SUCCESS, PaymentStatus.FAIL),
+                namedArguments(PaymentStatus.SUCCESS, PaymentStatus.UNKNOWN),
+                namedArguments(PaymentStatus.FAIL, PaymentStatus.NOT_STARTED),
+                namedArguments(PaymentStatus.FAIL, PaymentStatus.EXECUTING),
+                namedArguments(PaymentStatus.FAIL, PaymentStatus.SUCCESS),
+                namedArguments(PaymentStatus.FAIL, PaymentStatus.UNKNOWN),
+                namedArguments(PaymentStatus.UNKNOWN, PaymentStatus.NOT_STARTED),
+                namedArguments(PaymentStatus.UNKNOWN, PaymentStatus.SUCCESS),
+                namedArguments(PaymentStatus.UNKNOWN, PaymentStatus.FAIL)
+            );
+        }
+
+        private static Arguments namedArguments(PaymentStatus current, PaymentStatus next) {
+            return Arguments.of(
+                Named.of(current.name(), current),
+                Named.of(next.name(), next)
+            );
+        }
+    }
+
+}
