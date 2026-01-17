caa2f9708c3a2ce99a09e992ff2bdd2b657992c7
나제법 <skwpqjq@gmail.com>
2026-01-18 01:24:03 +0900

    refactor: Product toString 제거

--- src/main/java/com/ordertogether/paymentservice/payment/domain/Product.java
+++ src/main/java/com/ordertogether/paymentservice/payment/domain/Product.java
@@ -2,10 +2,8 @@ package com.ordertogether.paymentservice.payment.domain;
 
 import com.ordertogether.paymentservice.payment.domain.vo.Price;
 import lombok.Builder;
-import lombok.ToString;
 
 @Builder
-@ToString
 public record Product(
     Long id,
     String name,
