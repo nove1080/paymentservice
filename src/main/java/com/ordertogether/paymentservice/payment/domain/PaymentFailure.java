49059d813b43d6ea575fa0eb3cd172a82bd748f3
나제법 <skwpqjq@gmail.com>
2026-01-25 11:50:04 +0900

    refactor: PaymentFailure toString 중복 선언 제거

--- src/main/java/com/ordertogether/paymentservice/payment/domain/PaymentFailure.java
+++ src/main/java/com/ordertogether/paymentservice/payment/domain/PaymentFailure.java
@@ -1,10 +1,8 @@
 package com.ordertogether.paymentservice.payment.domain;
 
 import lombok.Builder;
-import lombok.ToString;
 
 @Builder
-@ToString
 public record PaymentFailure (
     String code,
     String message
