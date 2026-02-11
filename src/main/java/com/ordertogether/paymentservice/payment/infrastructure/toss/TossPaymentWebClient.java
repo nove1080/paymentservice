16337833e4c3a4171604380cdbea715d481d7481
나제법 <skwpqjq@gmail.com>
2026-02-11 16:31:27 +0900

    feat: 결제 승인 실패 시 재시도 로직 추가

--- src/main/java/com/ordertogether/paymentservice/payment/infrastructure/toss/TossPaymentWebClient.java
+++ src/main/java/com/ordertogether/paymentservice/payment/infrastructure/toss/TossPaymentWebClient.java
@@ -1,6 +1,9 @@
 package com.ordertogether.paymentservice.payment.infrastructure.toss;
 
-import com.ordertogether.paymentservice.payment.domain.PaymentFailure;
+import com.ordertogether.paymentservice.exception.PaymentGatewayConfirmationException;
+import com.ordertogether.paymentservice.exception.PaymentRetryExhaustedException;
+import com.ordertogether.paymentservice.payment.domain.PaymentStatus;
+import com.ordertogether.paymentservice.payment.infrastructure.toss.error.TossPaymentErrorCode;
 import com.ordertogether.paymentservice.payment.infrastructure.toss.mapper.TossPaymentMethodMapper;
 import com.ordertogether.paymentservice.payment.infrastructure.toss.request.TossPaymentConfirmRequest;
 import com.ordertogether.paymentservice.payment.infrastructure.toss.response.TossPaymentConfirmResponse;
@@ -12,6 +15,8 @@ import com.ordertogether.paymentservice.payment.service.result.PGConfirmResult.S
 import java.time.ZoneId;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.retry.RetryException;
+import org.springframework.core.retry.RetryTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
@@ -20,13 +25,41 @@ import org.springframework.web.client.RestClient;
 @RequiredArgsConstructor
 public class TossPaymentWebClient implements PaymentGatewayClient {
 
-    private final RestClient tossRestClient;
-
     private static final String CONFIRM_PAYMENT_URL = "/v1/payments/confirm";
     private static final String IDEMPOTENCY_KEY_HEADER = "Idempotency-Key";
 
+    private final RestClient tossRestClient;
+    private final RetryTemplate paymentGatewayRetryTemplate;
+
     @Override
-    public PGConfirmResult confirmPayment(PGConfirmCommand request) {
+    public PGConfirmResult confirmPayment(PGConfirmCommand request) throws PaymentRetryExhaustedException {
+        try {
+            return paymentGatewayRetryTemplate.execute(() -> executeConfirmPayment(request));
+        } catch (RetryException e) {
+            //RetryException -> BusinessException 으로 변환
+            String errorCode = PaymentStatus.UNKNOWN.name();
+            String errorMessage = PaymentStatus.EXECUTING.getDescription();
+            PaymentStatus paymentStatus = PaymentStatus.UNKNOWN;
+
+            if (e.getLastException() instanceof PaymentGatewayConfirmationException paymentGatewayConfirmationException) {
+                errorCode = paymentGatewayConfirmationException.getErrorCode();
+                errorMessage = paymentGatewayConfirmationException.getErrorMessage();
+                paymentStatus = paymentGatewayConfirmationException.getPaymentStatus();
+            }
+
+            throw new PaymentRetryExhaustedException(
+                request.paymentKey(),
+                request.orderId(),
+                errorCode,
+                errorMessage,
+                paymentStatus,
+                e.getRetryCount(),
+                e.getLastException()
+            );
+        }
+    }
+
+    private PGConfirmResult executeConfirmPayment(PGConfirmCommand request) throws PaymentGatewayConfirmationException {
         return tossRestClient.post()
             .uri(CONFIRM_PAYMENT_URL)
             .header(IDEMPOTENCY_KEY_HEADER, request.orderId().value())
@@ -38,17 +71,22 @@ public class TossPaymentWebClient implements PaymentGatewayClient {
             .exchange((req, res) -> {
                 if (res.getStatusCode().isError()) {
                     TossPaymentFailureResponse failureResponse = res.bodyTo(TossPaymentFailureResponse.class);
-                    return PGConfirmResult.builder()
-                        .paymentKey(request.paymentKey())
-                        .orderId(request.orderId())
-                        .status(TossPaymentErrorCode.from(failureResponse.code()).toPaymentStatus())
-                        .failureExtraInfo(PaymentFailure.builder()
-                            .code(failureResponse.code())
-                            .message(failureResponse.message())
-                            .build())
-                        .build();
+                    TossPaymentErrorCode errorCode = TossPaymentErrorCode.from(failureResponse == null ? TossPaymentErrorCode.UNKNOWN.name() : failureResponse.code());
+
+                    log.info("토스페이먼츠 결제 승인 실패. response: {}, errorCode: {}", failureResponse, errorCode);
+
+                    throw new PaymentGatewayConfirmationException(
+                        request.paymentKey(),
+                        request.orderId(),
+                        errorCode.toPaymentStatus(),
+                        errorCode.name(),
+                        errorCode.getMessage()
+                    );
                 } else {
                     TossPaymentConfirmResponse tossResponse = res.bodyTo(TossPaymentConfirmResponse.class);
+
+                    log.info("토스페이먼츠 결제 승인 성공. response: {}", tossResponse);
+
                     return PGConfirmResult.success(
                         request.paymentKey(),
                         request.orderId(),
