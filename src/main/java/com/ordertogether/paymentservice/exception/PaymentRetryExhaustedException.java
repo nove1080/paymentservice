c9864d925614e361785d237859597e5ac1cf0246
나제법 <skwpqjq@gmail.com>
2026-02-11 23:51:47 +0900

    refactor: 재시도 횟수 초과 시, 원인 예외 체인 추가

--- src/main/java/com/ordertogether/paymentservice/exception/PaymentRetryExhaustedException.java
+++ src/main/java/com/ordertogether/paymentservice/exception/PaymentRetryExhaustedException.java
@@ -3,14 +3,14 @@ package com.ordertogether.paymentservice.exception;
 import com.ordertogether.paymentservice.payment.domain.PaymentStatus;
 import com.ordertogether.paymentservice.payment.domain.vo.OrderId;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 /**
  * 결제 재시도 횟수 만료에 대한 예외
  */
 @Getter
-@RequiredArgsConstructor
 public class PaymentRetryExhaustedException extends RuntimeException {
+
+    private static final String MESSAGE = "결제 재시도 횟수가 초과되었습니다. [paymentKey = %s, orderId = %s, errorCode = %s, errorMessage = %s, paymentStatus = %s, retryCount = %d]";
 
     private final String paymentKey;
     private final OrderId orderId;
@@ -20,4 +20,16 @@ public class PaymentRetryExhaustedException extends RuntimeException {
     private final Integer retryCount;
     private final Throwable lastException;
 
+    public PaymentRetryExhaustedException(String paymentKey, OrderId orderId, String errorCode,
+        String errorMessage,
+        PaymentStatus paymentStatus, Integer retryCount, Throwable lastException) {
+        super(MESSAGE.formatted(paymentKey, orderId.value(), errorCode, errorMessage, paymentStatus.name(), retryCount), lastException);
+        this.paymentKey = paymentKey;
+        this.orderId = orderId;
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+        this.paymentStatus = paymentStatus;
+        this.retryCount = retryCount;
+        this.lastException = lastException;
+    }
 }
