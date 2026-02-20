1932a044b09c8450f8c811044b25dec33d97b43e
나제법 <skwpqjq@gmail.com>
2026-02-21 00:27:28 +0900

    refactor: JPAAuditing 설정 클래스 분리

---
+++ src/main/java/com/ordertogether/paymentservice/common/config/JpaAuditingConfiguration.java
@@ -0,0 +1,10 @@
+package com.ordertogether.paymentservice.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfiguration {
+
+}
