6668dcd22619b0075abad6b9c59be21977990e7c
나제법 <skwpqjq@gmail.com>
2026-03-13 23:36:23 +0900

    test: 결제 승인 서비스 통합 테스트케이스 추가
    - PG사의 결제 승인에 실패한 경우에 대한 검증 로직 추가
    - 중첩 클래스 사용 규칙 변경

--- src/test/java/com/ordertogether/paymentservice/payment/service/PaymentConfirmServiceIntegrationTest.java
+++ src/test/java/com/ordertogether/paymentservice/payment/service/PaymentConfirmServiceIntegrationTest.java
@@ -1,24 +1,29 @@
 package com.ordertogether.paymentservice.payment.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
 import com.ordertogether.paymentservice.payment.domain.PaymentEvent;
 import com.ordertogether.paymentservice.payment.domain.PaymentFailure;
 import com.ordertogether.paymentservice.payment.domain.PaymentMethod;
+import com.ordertogether.paymentservice.payment.domain.PaymentOrderHistory;
 import com.ordertogether.paymentservice.payment.domain.PaymentStatus;
 import com.ordertogether.paymentservice.payment.domain.vo.OrderId;
 import com.ordertogether.paymentservice.payment.domain.vo.Price;
+import com.ordertogether.paymentservice.payment.infrastructure.toss.error.TossPaymentErrorCode;
 import com.ordertogether.paymentservice.payment.repository.PaymentRepository;
 import com.ordertogether.paymentservice.payment.service.command.PaymentConfirmCommand;
 import com.ordertogether.paymentservice.payment.service.result.PGConfirmResult;
 import com.ordertogether.paymentservice.payment.service.result.PGConfirmResult.SuccessExtraInfo;
 import com.ordertogether.paymentservice.payment.service.result.PaymentConfirmResult;
 import com.ordertogether.paymentservice.support.base.PaymentIntegrationTest;
+import com.ordertogether.paymentservice.support.database.TransactionExecutor;
 import com.ordertogether.paymentservice.support.fixture.PaymentEventFixtureBuilder;
 import com.ordertogether.paymentservice.support.fixture.PaymentOrderFixtureBuilder;
 import java.time.LocalDateTime;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -26,7 +31,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.transaction.annotation.Transactional;
 
 @DisplayName("결제 승인 서비스 통합 테스트")
 @ActiveProfiles(profiles = "test")
@@ -38,115 +42,148 @@ class PaymentConfirmServiceIntegrationTest extends PaymentIntegrationTest {
     @Autowired
     private PaymentRepository paymentRepository;
 
+    @Autowired
+    private TransactionExecutor transactionExecutor;
+
     @MockitoBean
     private PaymentGatewayClient paymentGatewayClient;
 
+    private final OrderId orderId = OrderId.valueOf("test_order_id");
+    private final String paymentKey = "test_payment_key";
+    private final PaymentEvent paymentEvent = new PaymentEventFixtureBuilder()
+        .withPaymentKey(null)
+        .withMethod(null)
+        .withOrderId(orderId)
+        .withApprovedAt(null)
+        .addPaymentOrder(new PaymentOrderFixtureBuilder()
+            .withAmount(Price.valueOf(10000L))
+            .withPaymentStatus(PaymentStatus.NOT_STARTED)
+            .build())
+        .addPaymentOrder(new PaymentOrderFixtureBuilder()
+            .withAmount(Price.valueOf(20000L))
+            .withPaymentStatus(PaymentStatus.NOT_STARTED)
+            .build())
+        .build();
+
+    @BeforeEach
+    void saveNotStartedPayment() {
+        paymentRepository.insertPaymentEvent(paymentEvent);
+    }
+
     @Nested
-    @DisplayName("결제 승인 요청 시")
-    class WhenConfirmRequested {
+    @DisplayName("confirm()")
+    class Confirm {
 
-        private final OrderId orderId = OrderId.valueOf("test_order_id");
-        private final String paymentKey = "test_payment_key";
-        private final PaymentEvent paymentEvent = new PaymentEventFixtureBuilder()
-            .withPaymentKey(null)
-            .withMethod(null)
-            .withOrderId(orderId)
-            .withApprovedAt(null)
-            .addPaymentOrder(new PaymentOrderFixtureBuilder()
-                .withAmount(Price.valueOf(10000L))
-                .withPaymentStatus(PaymentStatus.NOT_STARTED)
-                .build())
-            .addPaymentOrder(new PaymentOrderFixtureBuilder()
-                .withAmount(Price.valueOf(20000L))
-                .withPaymentStatus(PaymentStatus.NOT_STARTED)
-                .build())
-            .build();
+        @Nested
+        @DisplayName("올바른 요청 시")
+        class WhenValidRequest {
 
-        @BeforeEach
-        void saveNotStartedPayment() {
-            paymentRepository.insertPaymentEvent(paymentEvent);
-        }
-
-        @Test
-        @DisplayName("결제 승인이 정상 처리된다")
-        void thenSuccess() {
-            //given
-            PaymentConfirmCommand command = PaymentConfirmCommand.builder()
-                .paymentKey(paymentKey)
-                .orderId(orderId)
-                .amount(30000L)
-                .build();
-
-            given(paymentGatewayClient.confirmPayment(any()))
-                .willReturn(PGConfirmResult.builder()
+            @Test
+            @DisplayName("결제를 승인한다")
+            void thenConfirmPayment() {
+                //given
+                PaymentConfirmCommand command = PaymentConfirmCommand.builder()
                     .paymentKey(paymentKey)
                     .orderId(orderId)
-                    .status(PaymentStatus.SUCCESS)
-                    .successExtraInfo(SuccessExtraInfo.builder()
-                        .approvedAt(LocalDateTime.now())
-                        .amount(paymentEvent.totalAmount())
-                        .paymentMethod(PaymentMethod.CARD)
-                        .build())
-                    .build());
+                    .amount(30000L)
+                    .build();
 
-            //when
-            PaymentConfirmResult confirmResult = paymentConfirmService.confirm(command);
+                given(paymentGatewayClient.confirmPayment(any()))
+                    .willReturn(PGConfirmResult.builder()
+                        .paymentKey(paymentKey)
+                        .orderId(orderId)
+                        .status(PaymentStatus.SUCCESS)
+                        .successExtraInfo(SuccessExtraInfo.builder()
+                            .approvedAt(LocalDateTime.now())
+                            .amount(paymentEvent.totalAmount())
+                            .paymentMethod(PaymentMethod.CARD)
+                            .build())
+                        .build());
 
-            //then
-            assertThat(confirmResult.paymentStatus()).isEqualTo(PaymentStatus.SUCCESS);
+                //when
+                PaymentConfirmResult confirmResult = paymentConfirmService.confirm(command);
+
+                //then
+                assertThat(confirmResult.paymentStatus()).isEqualTo(PaymentStatus.SUCCESS);
+            }
+
         }
 
-        @Test
-        @Transactional
-        @DisplayName("PG사 결제 승인에 실패한 경우, 결제 상태가 실패로 기록된다")
-        void givenPaymentGatewayFailResponse_thenFail() {
-            //given
-            PaymentConfirmCommand command = PaymentConfirmCommand.builder()
-                .paymentKey(paymentKey)
-                .orderId(orderId)
-                .amount(30000L)
-                .build();
+        @Nested
+        @DisplayName("결제 금액이 일치하지 않으면")
+        class WhenAmountIsInvalid {
 
-            given(paymentGatewayClient.confirmPayment(any()))
-                .willReturn(PGConfirmResult.builder()
+            @Test
+            @DisplayName("결제 승인을 거부한다")
+            void thenSavePaymentStatusFail() {
+                //given
+                PaymentConfirmCommand command = PaymentConfirmCommand.builder()
                     .paymentKey(paymentKey)
                     .orderId(orderId)
-                    .status(PaymentStatus.FAIL)
-                    .failureExtraInfo(PaymentFailure.builder()
-                        .code("")
-                        .message("")
-                        .build())
-                    .build());
+                    .amount(40000L)
+                    .build();
 
-            //when
-            PaymentConfirmResult confirmResult = paymentConfirmService.confirm(command);
+                //when
+                PaymentConfirmResult confirmResult = paymentConfirmService.confirm(command);
 
-            //then
-            PaymentEvent failedPaymentEvent = paymentRepository.selectPaymentEvent(orderId);
-
-            assertThat(confirmResult.paymentStatus().isFail()).isTrue();
-            assertThat(failedPaymentEvent.isFail()).isTrue();
+                //then
+                assertThat(confirmResult.paymentStatus().isFail()).isTrue();
+                transactionExecutor.invoke(
+                    () -> {
+                        PaymentEvent failedPaymentEvent = paymentRepository.selectPaymentEvent(orderId);
+                        assertThat(failedPaymentEvent.isFail()).isTrue();
+                    }
+                );
+            }
         }
 
-        @Test
-        @Transactional
-        @DisplayName("결제 금액이 상이한 경우, 결제 상태가 실패로 기록된다")
-        void whenInvalidPaymentErrorOccur_thenSavePaymentStatusFail() {
-            //given
-            PaymentConfirmCommand command = PaymentConfirmCommand.builder()
-                .paymentKey(paymentKey)
-                .orderId(orderId)
-                .amount(40000L)
-                .build();
+        @Nested
+        @DisplayName("PG사의 결제 승인에 실패한 경우")
+        class WhenPaymentConfirmFail {
 
-            //when
-            PaymentConfirmResult confirmResult = paymentConfirmService.confirm(command);
+            @Test
+            @DisplayName("결제 승인에 실패하고, 결제 상태와 실패 사유를 기록한다")
+            void givenPaymentGatewayFailResponse_thenFail() {
+                //given
+                PaymentConfirmCommand command = PaymentConfirmCommand.builder()
+                    .paymentKey(paymentKey)
+                    .orderId(orderId)
+                    .amount(30000L)
+                    .build();
 
-            //then
-            PaymentEvent failedPaymentEvent = paymentRepository.selectPaymentEvent(orderId);
+                TossPaymentErrorCode errorCode = TossPaymentErrorCode.FDS_ERROR;
+                given(paymentGatewayClient.confirmPayment(any()))
+                    .willReturn(PGConfirmResult.builder()
+                        .paymentKey(paymentKey)
+                        .orderId(orderId)
+                        .status(PaymentStatus.FAIL)
+                        .failureExtraInfo(PaymentFailure.builder()
+                            .code(errorCode.name())
+                            .message(errorCode.getMessage())
+                            .build())
+                        .build());
 
-            assertThat(confirmResult.paymentStatus().isFail()).isTrue();
-            assertThat(failedPaymentEvent.isFail()).isTrue();
+                //when
+                PaymentConfirmResult confirmResult = paymentConfirmService.confirm(command);
+
+                //then
+                assertThat(confirmResult.paymentStatus().isFail()).isTrue();
+                transactionExecutor.invoke(
+                    () -> {
+                        PaymentEvent failedPaymentEvent = paymentRepository.selectPaymentEvent(orderId);
+                        List<PaymentOrderHistory> paymentOrderHistories = paymentRepository.selectPaymentHistories(orderId);
+
+                        assertAll(
+                            () -> assertThat(failedPaymentEvent.isFail()).isTrue(),
+                            () -> assertThat(paymentOrderHistories)
+                                .filteredOn(it -> it.getPreviousStatus().isExecuting())
+                                .filteredOn(it -> it.getCurrentStatus().isFail())
+                                .allSatisfy(it -> assertThat(it.getReason()).isEqualTo(errorCode.getMessage()))
+                                .hasSize(paymentEvent.totalQuantity())
+                        );
+                    }
+                );
+            }
         }
 
     }
