d6a035f53276b834f745f4fef3570007ebb427f8
나제법 <skwpqjq@gmail.com>
2026-03-13 23:33:48 +0900

    feat: PaymentRepositoryJPAAdapter 결제 상태 조회 기능 추가

--- src/main/java/com/ordertogether/paymentservice/payment/infrastructure/jpa/PaymentOrderHistoryJPARepository.java
+++ src/main/java/com/ordertogether/paymentservice/payment/infrastructure/jpa/PaymentOrderHistoryJPARepository.java
@@ -1,10 +1,22 @@
 package com.ordertogether.paymentservice.payment.infrastructure.jpa;
 
 import com.ordertogether.paymentservice.payment.domain.PaymentOrderHistory;
+import com.ordertogether.paymentservice.payment.domain.vo.OrderId;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 interface PaymentOrderHistoryJPARepository extends JpaRepository<PaymentOrderHistory, Long> {
+
+    @Query("""
+        SELECT poh
+        FROM PaymentOrderHistory poh
+            JOIN PaymentOrder po on poh.paymentOrderId = po.id
+            JOIN PaymentEvent pe on po.paymentEvent.id = pe.id
+        WHERE pe.orderId = :orderId
+    """)
+    List<PaymentOrderHistory> findByOrderId(OrderId orderId);
 
 }
