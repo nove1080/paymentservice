08e4217e5d124dc89a655cfe79878424c0fb7350
나제법 <skwpqjq@gmail.com>
2026-03-13 23:33:26 +0900

    feat: PaymentRepository 결제 상태 기록 조회 메서드 추가

--- src/main/java/com/ordertogether/paymentservice/payment/repository/PaymentRepository.java
+++ src/main/java/com/ordertogether/paymentservice/payment/repository/PaymentRepository.java
@@ -3,6 +3,7 @@ package com.ordertogether.paymentservice.payment.repository;
 import com.ordertogether.paymentservice.payment.domain.PaymentEvent;
 import com.ordertogether.paymentservice.payment.domain.PaymentOrderHistory;
 import com.ordertogether.paymentservice.payment.domain.vo.OrderId;
+import java.util.List;
 
 public interface PaymentRepository {
 
@@ -11,4 +12,6 @@ public interface PaymentRepository {
     void insertPaymentEvent(PaymentEvent paymentEvent);
 
     void insertPaymentHistory(PaymentOrderHistory history);
+
+    List<PaymentOrderHistory> selectPaymentHistories(OrderId orderId);
 }
