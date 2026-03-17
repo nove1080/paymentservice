9f78f24f1b480c387f4708d684336bbb4280318f
나제법 <skwpqjq@gmail.com>
2026-03-18 01:35:00 +0900

    feat: 결제 복구 관련 설정값 정의

--- src/main/java/com/ordertogether/paymentservice/PaymentServiceApplication.java
+++ src/main/java/com/ordertogether/paymentservice/PaymentServiceApplication.java
@@ -2,7 +2,9 @@ package com.ordertogether.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class PaymentServiceApplication {
 
