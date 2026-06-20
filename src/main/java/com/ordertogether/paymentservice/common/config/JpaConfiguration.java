27dee75da1ba0ebfd617e78067e482d65603e767
나제법 <skwpqjq@gmail.com>
2026-06-20 22:49:48 +0900

    chore(config): JpaTransactionManager 수동 빈 등록
    - KafkaTransactionManager 와 중복 등록되어 겹치는 문제 해결

---
+++ src/main/java/com/ordertogether/paymentservice/common/config/JpaConfiguration.java
@@ -0,0 +1,27 @@
+package com.ordertogether.paymentservice.common.config;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfiguration {
+
+    @Primary
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+
+    @Bean
+    public TransactionTemplate transactionTemplate(PlatformTransactionManager transactionManager) {
+        return new TransactionTemplate(transactionManager);
+    }
+
+}
