b80f717008bc72592a956d470a652d8e7d44bc71
나제법 <skwpqjq@gmail.com>
2026-06-23 16:53:26 +0900

    refactor(PaymentEmailSender): 메일 전송 과정에서 try-catch 제거
    - exception 발생 시, 실패를 숨기지 않도록 하기 위함

--- src/main/java/com/ordertogether/paymentservice/mail/service/PaymentEmailSender.java
+++ src/main/java/com/ordertogether/paymentservice/mail/service/PaymentEmailSender.java
@@ -21,26 +21,21 @@ public class PaymentEmailSender {
 
     @Async
     public void sendDltAlertEmail(String originalTopic, String rawMessage, String exceptionMessage) {
-        try {
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setTo(adminEmail);
-            message.setSubject("[DLT 에러] 결제 메시지 처리 최종 실패");
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(adminEmail);
+        message.setSubject("[DLT 에러] 결제 메시지 처리 최종 실패");
 
-            String text = """
-                    결제 후속 처리 중 에러가 발생하여 메시지가 DLT로 이동되었습니다.
-                    발생 시간: %s
-                    원본 토픽: %s
-                    에러 원인: %s
-                    원본 데이터: %s
-                    확인 후 수동 재처리 바랍니다.
-                """.formatted(LocalDateTime.now(), originalTopic, exceptionMessage, rawMessage);
+        String text = """
+                결제 후속 처리 중 에러가 발생하여 메시지가 DLT로 이동되었습니다.
+                발생 시간: %sa
+                원본 토픽: %s
+                에러 원인: %s
+                원본 데이터: %s
+                확인 후 수동 재처리 바랍니다.
+            """.formatted(LocalDateTime.now(), originalTopic, exceptionMessage, rawMessage);
 
-            message.setText(text);
-            mailSender.send(message);
-            log.info("DLT Alert email 전송완료 수신자 {}", adminEmail);
-
-        } catch (Exception e) {
-            log.warn("DLT alert email 전송 실패", e);
-        }
+        message.setText(text);
+        mailSender.send(message);
+        log.info("DLT Alert email 전송완료 수신자 {}", adminEmail);
     }
 }
